537b214e625458d9c7b4b4a9bba13b7a65fa34d8
William Kearney <will@wskearney.com>
2024-04-02 19:59:10 +0200

    Return floats rather than doubles from the hash function

--- test/fillsinks.cpp
+++ test/fillsinks.cpp
@@ -14,28 +14,28 @@ extern "C" {
   rendering. Journal of Computer Graphics Techniques. Vol 9,
   No. 3. 21-38.
  */
-double pcg4d(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
-  uint64_t x = a * 0x5851f42d4c957f2d + 0x14057b7ef767814f;
-  uint64_t y = b * 0x5851f42d4c957f2d + 0x14057b7ef767814f;
-  uint64_t z = c * 0x5851f42d4c957f2d + 0x14057b7ef767814f;
-  uint64_t w = d * 0x5851f42d4c957f2d + 0x14057b7ef767814f;
+float pcg4d(uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
+  uint32_t x = a * 1664525u + 1013904223u;
+  uint32_t y = b * 1664525u + 1013904223u;
+  uint32_t z = c * 1664525u + 1013904223u;
+  uint32_t w = d * 1664525u + 1013904223u;
 
   x += y * w;
   y += z * x;
   z += x * y;
   w += y * z;
 
-  x ^= x >> 32;
-  y ^= y >> 32;
-  z ^= z >> 32;
-  w ^= w >> 32;
+  x ^= x >> 16;
+  y ^= y >> 16;
+  z ^= z >> 16;
+  w ^= w >> 16;
 
   x += y * w;
   y += z * x;
   z += x * y;
   w += y * z;
 
-  return (double)(w >> 11) * 0x1.0p-53;
+  return (float)(w >> 8) * 0x1.0p-24;
 }
 
 int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint64_t seed) {
@@ -57,7 +57,7 @@ int32_t random_dem_test(ptrdiff_t nrows, ptrdiff_t ncols, uint64_t seed) {
   for (ptrdiff_t col = 1; col < ncols - 1; col++) {
     for (ptrdiff_t row = 1; row < nrows - 1; row++) {
       // Each pixel of the filled raster should be >= the DEM
-      double z = output[col * nrows + row];
+      float z = output[col * nrows + row];
 
       if (z < dem[col * nrows + row]) {
         std::cout << "Pixel (" << row << ", " << col << ") is below the DEM"
