a4e04831e7b05314046072da2783d384a8ac0bef
bgailleton <boris.gailleton@gmail.com>
2024-07-29 16:34:30 +0200

    debug segfault (8)

--- src/graphflood/sfgraph.c
+++ src/graphflood/sfgraph.c
@@ -31,10 +31,10 @@ void compute_sfgraph(float* topo, int32_t* Sreceivers, int32_t* Sdonors, uint8_t
   
   // // Initialising the offset for neighbouring operations
   int32_t* offset = NULL;
-  (D8 == false) ? generate_offset_D4_flat(&offset,dim) : generate_offset_D8_flat(&offset, dim);
+  // (D8 == false) ? generate_offset_D4_flat(&offset,dim) : generate_offset_D8_flat(&offset, dim);
   // // Initialising the offset distance for each neighbour
   float* offdx = NULL;
-  (D8 == false) ? generate_offsetdx_D4(&offdx,dx) : generate_offsetdx_D8(&offdx,dx);
+  // (D8 == false) ? generate_offsetdx_D4(&offdx,dx) : generate_offsetdx_D8(&offdx,dx);
 
   // printf("DEBUG::OFFSET::%s\n", offset[0]);
 
@@ -50,7 +50,7 @@ void compute_sfgraph(float* topo, int32_t* Sreceivers, int32_t* Sdonors, uint8_t
       printf( "%s\n", node);
 
       // By convention (see fastscape, LSDTT, ...) a no steepest receiver = itself
-      // Sreceivers[node] = node;
+      Sreceivers[node] = node;
       continue;
 
       // Boundary condition checks: the node needs to being able to give to have receivers
