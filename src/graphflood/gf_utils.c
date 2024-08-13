2320a30a20e0b9f19c4487ca2cf0dece1f553bf6
bgailleton <boris.gailleton@gmail.com>
2024-08-13 10:55:15 +0200

    Apply clang-format

--- src/graphflood/gf_utils.c
+++ src/graphflood/gf_utils.c
@@ -10,17 +10,17 @@ Row major representation
 |1|x|2|
 | |3| |
 */
-void generate_offset_D4(int8_t* off0, int8_t* off1){
-	// creating a generic array to assign to the pointer
-	off0[0] = -1;
-	off0[1] = 0;
-	off0[2] = 0;
-	off0[3] = -1;
-	// creating a generic array to assign to the pointer
-	off1[0] = 0;
-	off1[1] = -1;
-	off1[2] = 1;
-	off1[3] = 0;
+void generate_offset_D4(int8_t* off0, int8_t* off1) {
+  // creating a generic array to assign to the pointer
+  off0[0] = -1;
+  off0[1] = 0;
+  off0[2] = 0;
+  off0[3] = -1;
+  // creating a generic array to assign to the pointer
+  off1[0] = 0;
+  off1[1] = -1;
+  off1[2] = 1;
+  off1[3] = 0;
 }
 
 /*
@@ -31,16 +31,16 @@ Row major representation
 |1|x|2|
 | |3| |
 */
-void generate_offsetdx_D4(GF_FLOAT* off, GF_FLOAT dx){
-	// creating a generic array to assign to the pointer
-	off[0] = dx;
-	off[1] = dx;
-	off[2] = dx;
-	off[3] = dx;
+void generate_offsetdx_D4(GF_FLOAT* off, GF_FLOAT dx) {
+  // creating a generic array to assign to the pointer
+  off[0] = dx;
+  off[1] = dx;
+  off[2] = dx;
+  off[3] = dx;
 }
 
 /*
-Generate the offsets for neighbouring operations 
+Generate the offsets for neighbouring operations
 D8 version:
 
 Row major representation
@@ -48,25 +48,25 @@ Row major representation
 |3|x|4|
 |5|6|7|
 */
-void generate_offset_D8(int8_t* off0, int8_t* off1){
-	// creating a generic array to assign to the pointer
-	off0[0] = -1;
-	off0[1] = -1;
-	off0[2] = -1;
-	off0[3] = 0 ;
-	off0[4] = 0 ;
-	off0[5] = 1 ;
-	off0[6] = 1 ;
-	off0[7] = 1 ;
-	// creating a generic array to assign to the pointer
-	off1[0] = -1;
-	off1[1] = 0 ;
-	off1[2] = 1 ;
-	off1[3] = -1;
-	off1[4] = 1 ;
-	off1[5] = -1;
-	off1[6] = 0 ;
-	off1[7] = 1 ;
+void generate_offset_D8(int8_t* off0, int8_t* off1) {
+  // creating a generic array to assign to the pointer
+  off0[0] = -1;
+  off0[1] = -1;
+  off0[2] = -1;
+  off0[3] = 0;
+  off0[4] = 0;
+  off0[5] = 1;
+  off0[6] = 1;
+  off0[7] = 1;
+  // creating a generic array to assign to the pointer
+  off1[0] = -1;
+  off1[1] = 0;
+  off1[2] = 1;
+  off1[3] = -1;
+  off1[4] = 1;
+  off1[5] = -1;
+  off1[6] = 0;
+  off1[7] = 1;
 }
 
 /*
@@ -77,84 +77,80 @@ Row major representation
 |1|x|2|
 | |3| |
 */
-void generate_offsetdx_D8(GF_FLOAT* off, GF_FLOAT dx){
-	GF_FLOAT diag = sqrt(2) * dx;
-	// creating a generic array to assign to the pointer
-	off[0] = diag;
-	off[1] = dx;
-	off[2] = diag;
-	off[3] = dx;
-	off[4] = dx;
-	off[5] = diag;
-	off[6] = dx;
-	off[7] = diag;
+void generate_offsetdx_D8(GF_FLOAT* off, GF_FLOAT dx) {
+  GF_FLOAT diag = sqrt(2) * dx;
+  // creating a generic array to assign to the pointer
+  off[0] = diag;
+  off[1] = dx;
+  off[2] = diag;
+  off[3] = dx;
+  off[4] = dx;
+  off[5] = diag;
+  off[6] = dx;
+  off[7] = diag;
 }
 
 /*
 Generate the flat offsets for neighbouring operations
 D4 version.
 */
-void generate_offset_D4_flat(GF_INT* off, GF_UINT* dim){
-	// creating a generic array to assign to the pointer
-	off[0] = -dim[1];
-	off[1] = -1;
-	off[2] = 1;
-	off[3] = dim[1];
+void generate_offset_D4_flat(GF_INT* off, GF_UINT* dim) {
+  // creating a generic array to assign to the pointer
+  off[0] = -dim[1];
+  off[1] = -1;
+  off[2] = 1;
+  off[3] = dim[1];
 }
 
 /*
 Generate the flat offsets for neighbouring operations
 D8 version.
 */
-void generate_offset_D8_flat(GF_INT* off, GF_UINT* dim){
-	// creating a generic array to assign to the pointer
-	off[0] = -dim[1] - 1;
-	off[1] = -dim[1] + 0;
-	off[2] = -dim[1] + 1;
-	off[3] = -1;
-	off[4] = 1 ;
-	off[5] = dim[1] - 1;
-	off[6] = dim[1] + 0 ;
-	off[7] = dim[1] + 1 ;
+void generate_offset_D8_flat(GF_INT* off, GF_UINT* dim) {
+  // creating a generic array to assign to the pointer
+  off[0] = -dim[1] - 1;
+  off[1] = -dim[1] + 0;
+  off[2] = -dim[1] + 1;
+  off[3] = -1;
+  off[4] = 1;
+  off[5] = dim[1] - 1;
+  off[6] = dim[1] + 0;
+  off[7] = dim[1] + 1;
 }
-
-
-
 
 /*
 returns the flat index of a node from its dimensions
 
 Example in row major:
-this_dim0 is the current row, dim[1] is the number of columns and this_dim1 is the current column
+this_dim0 is the current row, dim[1] is the number of columns and this_dim1 is
+the current column
 
 */
 // GF_INT dim2flat(GF_INT this_dim0, GF_INT this_dim1, GF_UINT* dim){
 // 	return this_dim0 * dim[1] + this_dim1;
 // }
-GF_UINT dim2flat(GF_UINT this_dim0, GF_UINT this_dim1, GF_UINT* dim){
-	return (this_dim0 * dim[1] + this_dim1);
+GF_UINT dim2flat(GF_UINT this_dim0, GF_UINT this_dim1, GF_UINT* dim) {
+  return (this_dim0 * dim[1] + this_dim1);
 }
 
-
-void flat2dim(GF_UINT node, GF_UINT* this_dim0, GF_UINT* this_dim1, GF_UINT* dim){
-	*this_dim0 = node % dim[1];
-	*this_dim1 = node - (*this_dim0) * dim[1];
+void flat2dim(GF_UINT node, GF_UINT* this_dim0, GF_UINT* this_dim1,
+              GF_UINT* dim) {
+  *this_dim0 = node % dim[1];
+  *this_dim1 = node - (*this_dim0) * dim[1];
 }
 
 /*
 Return the number of neighbours
 depending on the DX topology
 */
-uint8_t N_neighbour(bool D8){
-	if(D8)
-		return 8;
-	else
-		return 4;
+uint8_t N_neighbour(bool D8) {
+  if (D8)
+    return 8;
+  else
+    return 4;
 }
 
-GF_UINT nxy(GF_UINT* dim){return dim[0] * dim[1];}
-
-
+GF_UINT nxy(GF_UINT* dim) { return dim[0] * dim[1]; }
 
 /*
 The following functions helps determining how a given node handles flux.
@@ -173,8 +169,8 @@ FLOW = 1,
 2 is legacy and does not do anything
 
 Flow can out there but can also flow to downstream neighbours.
-For example, a node at the edge - but that have a neighbour with lower elevation:
-CAN_OUT = 3,
+For example, a node at the edge - but that have a neighbour with lower
+elevation: CAN_OUT = 3,
 
 Flow can only out when entering the cell:
 OUT = 4,
@@ -182,8 +178,8 @@ OUT = 4,
 PROBABLY LEGACY
 FORCE_OUT = 5,
 
-For cells located at the edge of a DEM, flow can pass through but not leave (local minima if no downstream neighbour)
-CANNOT_OUT = 6,
+For cells located at the edge of a DEM, flow can pass through but not leave
+(local minima if no downstream neighbour) CANNOT_OUT = 6,
 
 Flow can only flow to potential receivers
 exemple, you have a DEM of a river segment and want to input water from a side:
@@ -200,119 +196,98 @@ uint8_t has 256 possibilities so there is plenty of space for more options
 */
 
 /*
-I know BCs is not used so far here but that's future proof for periodic conditions or other edge cases
+I know BCs is not used so far here but that's future proof for periodic
+conditions or other edge cases
 */
-void check_bound(GF_UINT node, GF_UINT* dim, uint8_t* BCs, bool* valid ){
-	if(node >= dim[0] * dim[1])
-		*valid = false;
+void check_bound(GF_UINT node, GF_UINT* dim, uint8_t* BCs, bool* valid) {
+  if (node >= dim[0] * dim[1]) *valid = false;
 }
 
-void check_top_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs, bool* valid, bool D8){
-
-	// Checking if the node is on the first row and trying to target above
-	if (node < dim[1]) {
-		if( (D8 && n < 3) || (D8 == false && n == 0))
-			*valid = false;
-	}
-	// # Done
+void check_top_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs,
+                       bool* valid, bool D8) {
+  // Checking if the node is on the first row and trying to target above
+  if (node < dim[1]) {
+    if ((D8 && n < 3) || (D8 == false && n == 0)) *valid = false;
+  }
+  // # Done
 }
 
-void check_bottom_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs, bool* valid, bool D8){
-
-	// Checking if the node is on the first row and trying to target above
-	if (node >= dim[0] * dim[1] - dim[1]) {
-		if( (D8 && n >= 5) || (D8 == false && n == 3))
-			*valid = false;
-	}
-	// # Done
+void check_bottom_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs,
+                          bool* valid, bool D8) {
+  // Checking if the node is on the first row and trying to target above
+  if (node >= dim[0] * dim[1] - dim[1]) {
+    if ((D8 && n >= 5) || (D8 == false && n == 3)) *valid = false;
+  }
+  // # Done
 }
 
-void check_left_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs, bool* valid, bool D8){
-
-	// Checking if the node is on the first row and trying to target above
-	if (node % dim[1] == 0) {
-		if( (D8 && (n == 0 || n == 3 || n == 5) ) || (D8 == false && n == 1))
-			*valid = false;
-	}
-	// # Done
+void check_left_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs,
+                        bool* valid, bool D8) {
+  // Checking if the node is on the first row and trying to target above
+  if (node % dim[1] == 0) {
+    if ((D8 && (n == 0 || n == 3 || n == 5)) || (D8 == false && n == 1))
+      *valid = false;
+  }
+  // # Done
 }
 
-void check_right_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs, bool* valid, bool D8){
-
-	// Checking if the node is on the first row and trying to target above
-	if (node % dim[1] == dim[1] - 1) {
-		if( (D8 && (n == 2 || n == 4 || n == 7) ) || (D8 == false && n == 2))
-			*valid = false;
-	}
-	// # Done
+void check_right_customs(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs,
+                         bool* valid, bool D8) {
+  // Checking if the node is on the first row and trying to target above
+  if (node % dim[1] == dim[1] - 1) {
+    if ((D8 && (n == 2 || n == 4 || n == 7)) || (D8 == false && n == 2))
+      *valid = false;
+  }
+  // # Done
 }
 
-
-bool check_bound_neighbour(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs, bool D8){
-	bool valid = true;
-	check_bound(node, dim, BCs, &valid );
-	check_top_customs(node, n, dim, BCs, &valid, D8);
-	check_bottom_customs(node, n, dim, BCs, &valid, D8);
-	check_left_customs(node, n, dim, BCs, &valid, D8);
-	check_right_customs(node, n, dim, BCs, &valid, D8);
-	return valid;
+bool check_bound_neighbour(GF_UINT node, uint8_t n, GF_UINT* dim, uint8_t* BCs,
+                           bool D8) {
+  bool valid = true;
+  check_bound(node, dim, BCs, &valid);
+  check_top_customs(node, n, dim, BCs, &valid, D8);
+  check_bottom_customs(node, n, dim, BCs, &valid, D8);
+  check_left_customs(node, n, dim, BCs, &valid, D8);
+  check_right_customs(node, n, dim, BCs, &valid, D8);
+  return valid;
 }
 
+bool can_receive(GF_UINT node, uint8_t* BCs) {
+  if (BCs[node] == 1 || BCs[node] == 3 || BCs[node] == 4 || BCs[node] == 5 ||
+      BCs[node] == 6 || BCs[node] == 7 || BCs[node] == 9
 
-bool can_receive(GF_UINT node, uint8_t* BCs){
-	if(
-		BCs[node] == 1 ||
-		BCs[node] == 3 ||
-		BCs[node] == 4 ||
-		BCs[node] == 5 ||
-		BCs[node] == 6 ||
-		BCs[node] == 7 ||
-		BCs[node] == 9
-
-	){
-		return true;
-	}else{
-		return false;
-	}
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-bool can_give(GF_UINT node, uint8_t* BCs){
-	if(
-		BCs[node] == 1 ||
-		BCs[node] == 3 ||
-		BCs[node] == 6 ||
-		BCs[node] == 7 ||
-		BCs[node] == 8 ||
-		BCs[node] == 9
+bool can_give(GF_UINT node, uint8_t* BCs) {
+  if (BCs[node] == 1 || BCs[node] == 3 || BCs[node] == 6 || BCs[node] == 7 ||
+      BCs[node] == 8 || BCs[node] == 9
 
-	){
-		return true;
-	}else{
-		return false;
-	}
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-bool can_out(GF_UINT node, uint8_t* BCs){
-	if(
-		BCs[node] == 3 ||
-		BCs[node] == 4 ||
-		BCs[node] == 5
+bool can_out(GF_UINT node, uint8_t* BCs) {
+  if (BCs[node] == 3 || BCs[node] == 4 || BCs[node] == 5
 
-
-	){
-		return true;
-	}else{
-		return false;
-	}
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
-bool is_nodata(GF_UINT node, uint8_t* BCs){
-	if(
-		BCs[node] == 0
-	){
-		return true;
-	}else{
-		return false;
-	}
+bool is_nodata(GF_UINT node, uint8_t* BCs) {
+  if (BCs[node] == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- src/graphflood/priority_flood_standalone.c
+++ src/graphflood/priority_flood_standalone.c
@@ -1,23 +1,21 @@
 /*
 This file contains routine to build a single flow graph.
-A Single flow graph is a data structure describing a Directed Acyclic Graph (DAG)
-where nodes only have one receiver max, usually the steepest gradient one.
+A Single flow graph is a data structure describing a Directed Acyclic Graph
+(DAG) where nodes only have one receiver max, usually the steepest gradient one.
 
 */
 
 #define TOPOTOOLBOX_BUILD
 
-#include "graphflood/define_types.h"
-
-
+#include <float.h>
 #include <math.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <float.h>
 
 #include "../morphology/reconstruct.h"
 #include "gf_utils.h"
+#include "graphflood/define_types.h"
 #include "pq_priority_flood.h"
 #include "queue_pit.h"
 #include "topotoolbox.h"
@@ -29,215 +27,198 @@ Fills it in place, and take into D8/D4 topology and custom boundary conditions
 */
 TOPOTOOLBOX_API
 void compute_priority_flood(float* topo, uint8_t* BCs, GF_UINT* dim, bool D8) {
-	
-	// Initialising the offset for neighbouring operations
-	GF_INT offset[8];
-	(D8 == false) ? generate_offset_D4_flat(offset,dim) : generate_offset_D8_flat(offset, dim);
+  // Initialising the offset for neighbouring operations
+  GF_INT offset[8];
+  (D8 == false) ? generate_offset_D4_flat(offset, dim)
+                : generate_offset_D8_flat(offset, dim);
 
+  // initialising the nodes to not closed ( = to be processed)
+  uint8_t* closed = (uint8_t*)malloc(nxy(dim) * sizeof(uint8_t));
+  for (GF_UINT i = 0; i < nxy(dim); ++i) closed[i] = false;
 
-	// initialising the nodes to not closed ( = to be processed)
-	uint8_t* closed = (uint8_t*) malloc( nxy(dim) * sizeof(uint8_t) );
-	for(GF_UINT i=0; i<nxy(dim); ++i)
-		closed[i]=false;
+  // PitQueue is a FIFO data structure to fill pits without having to use the
+  // more expensive priority queue
+  PitQueue pit;
+  pitqueue_init(&pit, nxy(dim));
 
+  // The priority queue data structure (keeps stuff sorted)
+  PFPQueue open;
+  pfpq_init(&open, nxy(dim));
 
-	// PitQueue is a FIFO data structure to fill pits without having to use the more expensive priority queue
-	PitQueue pit;
-	pitqueue_init(&pit,nxy(dim));
+  // temp variable to help with PitQueue
+  float PitTop = FLT_MIN;
 
-	// The priority queue data structure (keeps stuff sorted)
-	PFPQueue open;
-	pfpq_init(&open, nxy(dim));
+  // Initialisation phase: initialise the queue with nodethat can drain out of
+  // the model Also initialise the sfg data structure
+  for (GF_UINT i = 0; i < nxy(dim); ++i) {
+    // If flow can leave, I push
+    if (can_out(i, BCs)) {
+      pfpq_push(&open, i, topo[i]);
+      closed[i] = true;
+    }
 
+    // Note that no data node are immediately closed as processed
+    if (is_nodata(i, BCs)) {
+      closed[i] = true;
+    }
+  }
 
-	// temp variable to help with PitQueue
-	float PitTop = FLT_MIN;
+  // Here we go: Starting the main process
+  // Processing stops once all the nodes - nodata have been visited once (i.e.
+  // pit fifo and PQ empty)
+  GF_UINT node;
+  while (pfpq_empty(&open) == false || pit.size > 0) {
+    // Selecting the next node
+    if (pit.size > 0 && pfpq_empty(&open) == false &&
+        pfpq_top_priority(&open) == topo[pit.front]) {
+      node = pfpq_pop_and_get_key(&open);
+      PitTop = FLT_MIN;
 
-	// Initialisation phase: initialise the queue with nodethat can drain out of the model
-	// Also initialise the sfg data structure
-	for(GF_UINT i=0; i<nxy(dim); ++i){
+    } else if (pit.size > 0) {
+      node = pitqueue_pop_and_get(&pit);
+      if (PitTop == FLT_MIN) PitTop = topo[node];
+    } else {
+      node = pfpq_pop_and_get_key(&open);
+      PitTop = FLT_MIN;
+    }
 
-		// If flow can leave, I push
-		if(can_out(i,BCs)){
-			pfpq_push(&open, i, topo[i]);
-			closed[i] = true;
-		}
+    // for all the neighbours ...
+    for (GF_UINT n = 0; n < N_neighbour(D8); ++n) {
+      // Checking if the neighbour belongs to the grid
+      if (check_bound_neighbour(node, n, dim, BCs, D8) == false) {
+        continue;
+      }
 
-		// Note that no data node are immediately closed as processed
-		if(is_nodata(i,BCs)){
-			closed[i] = true;
-		}
+      // flat indices
+      GF_UINT nnode = node + offset[n];
 
-	}
+      // if nodata I skip
+      if (is_nodata(nnode, BCs)) continue;
 
+      // If the node is closed (i.e. already in a pit or processed) I skip
+      if (closed[nnode] == false) {
+        // other wise I close it
+        closed[nnode] = true;
 
-	// Here we go: Starting the main process
-	// Processing stops once all the nodes - nodata have been visited once (i.e. pit fifo and PQ empty)
-	GF_UINT node;
-	while(pfpq_empty(&open) == false || pit.size>0){
+        // I raise its elevation if is in pit
+        // nextafter maskes sure I pick the next floating point data
+        // corresponding to the current precision
+        if (topo[nnode] <= nextafter(topo[node], FLT_MAX)) {
+          // raise
+          topo[nnode] = nextafter(topo[node], FLT_MAX);
+          // put in pit queue
+          pitqueue_enqueue(&pit, nnode);
+          // Affect current node as neighbours Sreceiver
+        } else {
+          // ... Not in a pit? then in PQ for next proc
+          pfpq_push(&open, nnode, topo[nnode]);
+        }
+      }
+    }
+  }
 
-
-		// Selecting the next node
-		if(pit.size>0 && pfpq_empty(&open)==false && pfpq_top_priority(&open) == topo[pit.front]){
-
-			node=pfpq_pop_and_get_key(&open);
-			PitTop=FLT_MIN;
-		
-		} else if(pit.size>0){
-
-			node=pitqueue_pop_and_get(&pit);
-			if(PitTop==FLT_MIN)
-				PitTop=topo[node];
-		} else {
-			node=pfpq_pop_and_get_key(&open);
-			PitTop=FLT_MIN;
-		}
-
-		// for all the neighbours ...
-		for(GF_UINT n = 0; n<N_neighbour(D8); ++n){
-
-			// Checking if the neighbour belongs to the grid
-			if(check_bound_neighbour(node, n, dim, BCs, D8) == false){
-				continue;
-			}
-
-			// flat indices
-			GF_UINT nnode = node + offset[n];
-
-			// if nodata I skip
-			if(is_nodata(nnode,BCs)) continue;
-
-			// If the node is closed (i.e. already in a pit or processed) I skip
-			if(closed[nnode] == false){
-
-				// other wise I close it
-				closed[nnode] = true;
-
-				// I raise its elevation if is in pit
-				// nextafter maskes sure I pick the next floating point data corresponding to the current precision 
-				if(topo[nnode] <= nextafter(topo[node],FLT_MAX)){
-					// raise
-					topo[nnode] = nextafter(topo[node],FLT_MAX);
-					// put in pit queue
-					pitqueue_enqueue(&pit,nnode);
-					// Affect current node as neighbours Sreceiver
-				} else{
-					// ... Not in a pit? then in PQ for next proc
-					pfpq_push(&open,nnode,topo[nnode]);
-				}
-			}
-
-		}
-
-	}
-
-	// Done with the queues and close, free memory
-	pfpq_free(&open);
-	pitqueue_free(&pit);
-	free(closed);
-
+  // Done with the queues and close, free memory
+  pfpq_free(&open);
+  pitqueue_free(&pit);
+  free(closed);
 }
-
-
 
 /*
 See topotoolbox.h for details
-Variant of priority flood + epsilon that also computes a multiple flow compatible topological ordering (Stack) on the go
-Note: this version is a bit slower as it needs to push all nodes in the priority queue including the ones in pit (no fifo queue)
-By experience the slowing down factor is small for most cases ( low amount of depressions, ~10% max slow down)
+Variant of priority flood + epsilon that also computes a multiple flow
+compatible topological ordering (Stack) on the go Note: this version is a bit
+slower as it needs to push all nodes in the priority queue including the ones in
+pit (no fifo queue) By experience the slowing down factor is small for most
+cases ( low amount of depressions, ~10% max slow down)
 */
 TOPOTOOLBOX_API
-void compute_priority_flood_plus_topological_ordering(float* topo, GF_UINT* stack, uint8_t* BCs, GF_UINT* dim, bool D8) {
+void compute_priority_flood_plus_topological_ordering(float* topo,
+                                                      GF_UINT* stack,
+                                                      uint8_t* BCs,
+                                                      GF_UINT* dim, bool D8) {
+  // Initialising the offset for neighbouring operations
+  GF_INT offset[8];
+  (D8 == false) ? generate_offset_D4_flat(offset, dim)
+                : generate_offset_D8_flat(offset, dim);
 
-	// Initialising the offset for neighbouring operations
-	GF_INT offset[8];
-	(D8 == false) ? generate_offset_D4_flat(offset,dim) : generate_offset_D8_flat(offset, dim);
+  // initialising the nodes to not closed ( = to be processed)
+  uint8_t* closed = (uint8_t*)malloc(nxy(dim) * sizeof(uint8_t));
+  for (GF_UINT i = 0; i < nxy(dim); ++i) closed[i] = false;
 
-	// initialising the nodes to not closed ( = to be processed)
-	uint8_t* closed = (uint8_t*) malloc( nxy(dim) * sizeof(uint8_t) );
-	for(GF_UINT i=0; i<nxy(dim); ++i)
-		closed[i]=false;
+  // The priority queue data structure (keeps stuff sorted)
+  PFPQueue open;
+  pfpq_init(&open, nxy(dim));
 
-	// The priority queue data structure (keeps stuff sorted)
-	PFPQueue open;
-	pfpq_init(&open, nxy(dim));
+  GF_UINT istack = 0;
 
-	GF_UINT istack = 0;
+  // Initialisation phase: initialise the queue with nodethat can drain out of
+  // the model Also initialise the sfg data structure
+  for (GF_UINT i = 0; i < nxy(dim); ++i) {
+    // If flow can leave, I push
+    if (can_out(i, BCs)) {
+      pfpq_push(&open, i, topo[i]);
+      closed[i] = true;
+    }
 
-	// Initialisation phase: initialise the queue with nodethat can drain out of the model
-	// Also initialise the sfg data structure
-	for(GF_UINT i=0; i<nxy(dim); ++i){
+    // Note that no data node are immediately closed as processed
+    if (is_nodata(i, BCs)) {
+      closed[i] = true;
+      stack[istack] = i;
+      ++istack;
+    }
+  }
 
-		// If flow can leave, I push
-		if(can_out(i,BCs)){
-			pfpq_push(&open, i, topo[i]);
-			closed[i] = true;
-		}
+  // Here we go: Starting the main process
+  // Processing stops once all the nodes - nodata have been visited once (i.e.
+  // pit fifo and PQ empty)
+  GF_UINT node;
+  while (pfpq_empty(&open) == false) {
+    // printf("DEBUG::A3\n");
+    node = pfpq_pop_and_get_key(&open);
 
-		// Note that no data node are immediately closed as processed
-		if(is_nodata(i,BCs)){
-			closed[i] = true;
-			stack[istack] = i;
-			++istack;
-		}
+    // printf("%u vs %u\n", istack, nxy(dim));
+    if (istack < nxy(dim)) {
+      stack[istack] = node;
+    }
 
-	}
+    ++istack;
 
+    // for all the neighbours ...
+    for (GF_UINT n = 0; n < N_neighbour(D8); ++n) {
+      // Checking if the neighbour belongs to the grid
+      if (check_bound_neighbour(node, n, dim, BCs, D8) == false) {
+        continue;
+      }
 
-	// Here we go: Starting the main process
-	// Processing stops once all the nodes - nodata have been visited once (i.e. pit fifo and PQ empty)
-	GF_UINT node;
-	while(pfpq_empty(&open) == false){
+      // flat indices
+      GF_UINT nnode = node + offset[n];
 
-		// printf("DEBUG::A3\n");
-		node=pfpq_pop_and_get_key(&open);
-		
-		// printf("%u vs %u\n", istack, nxy(dim));
-		if(istack < nxy(dim)){
-			stack[istack] = node;
-		}
+      // if nodata I skip
+      if (is_nodata(nnode, BCs)) continue;
 
-		++istack;
+      // If the node is closed (i.e. already in a pit or processed) I skip
+      if (closed[nnode] == false) {
+        // other wise I close it
+        closed[nnode] = true;
 
-		// for all the neighbours ...
-		for(GF_UINT n = 0; n<N_neighbour(D8); ++n){
+        // I raise its elevation if is in pit
+        // nextafter maskes sure I pick the next floating point data
+        // corresponding to the current precision
+        if (topo[nnode] <= nextafter(topo[node], FLT_MAX) + 1e-4) {
+          // raise
+          topo[nnode] = nextafter(topo[node], FLT_MAX) + 1e-4;
+          // put in pqueue
+          pfpq_push(&open, nnode, topo[nnode]);
+          // Affect current node as neighbours Sreceiver
+        } else {
+          // ... Not in a pit? then wimply in PQ for next proc
+          pfpq_push(&open, nnode, topo[nnode]);
+        }
+      }
+    }
+  }
 
-			// Checking if the neighbour belongs to the grid
-			if(check_bound_neighbour(node, n, dim, BCs, D8) == false){
-				continue;
-			}
-
-			// flat indices
-			GF_UINT nnode = node + offset[n];
-
-			// if nodata I skip
-			if(is_nodata(nnode,BCs)) continue;
-
-			// If the node is closed (i.e. already in a pit or processed) I skip
-			if(closed[nnode] == false){
-
-				// other wise I close it
-				closed[nnode] = true;
-
-				// I raise its elevation if is in pit
-				// nextafter maskes sure I pick the next floating point data corresponding to the current precision 
-				if(topo[nnode] <= nextafter(topo[node],FLT_MAX) + 1e-4){
-					// raise
-					topo[nnode] = nextafter(topo[node],FLT_MAX) + 1e-4;
-					// put in pqueue
-					pfpq_push(&open,nnode,topo[nnode]);
-					// Affect current node as neighbours Sreceiver
-				} else{
-					// ... Not in a pit? then wimply in PQ for next proc
-					pfpq_push(&open,nnode,topo[nnode]);
-				}
-			}
-
-		}
-	}
-
-	// Done with the queues and close, free memory
-	pfpq_free(&open);
-	free(closed);
-
+  // Done with the queues and close, free memory
+  pfpq_free(&open);
+  free(closed);
 }
