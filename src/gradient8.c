1309c8c55016b0cda26282865b4b7da58de2a253
Teschl <69400012+Teschl@users.noreply.github.com>
2024-09-03 14:59:00 +0200

    Remove unit conversion

--- src/gradient8.c
+++ src/gradient8.c
@@ -1,13 +1,16 @@
 #define TOPOTOOLBOX_BUILD
 
 #include <math.h>
-#include <omp.h>
 #include <stddef.h>
+
+#if TOPOTOOLBOX_OPENMP_VERSION > 0
+#include <omp.h>
+#endif
+
 #include <stdint.h>
 
 #include "topotoolbox.h"
 
-#define PI 3.14159265358979323846f
 #define SQRT2 1.41421356237309504880f
 /*
   Compute the gradient of each cell in a digital elevation model.
@@ -20,22 +23,15 @@
   int use_mp:
     0   --> don't use multiprocessing
     1   --> use multiprocessing
-
-  char unit:
-    't' --> tangent (default)
-    'r' --> radian
-    'd' --> degree
-    's' --> sine
-    'p' --> percent
  */
 
 TOPOTOOLBOX_API
-void gradient8(float *output, float *dem, float cellsize, char unit, int use_mp,
+void gradient8(float *output, float *dem, float cellsize, int use_mp,
                ptrdiff_t dims[2]) {
   ptrdiff_t i_offset[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
   ptrdiff_t j_offset[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-#ifdef _MSC_VER
+#if TOPOTOOLBOX_OPENMP_VERSION < 30
   ptrdiff_t j;
 #pragma omp parallel for if (use_mp)
   for (j = 0; j < dims[1]; j++) {
@@ -67,30 +63,15 @@ void gradient8(float *output, float *dem, float cellsize, char unit, int use_mp,
           vertical_dist = fabsf(dem[neighbour_j * dims[0] + neighbour_i] -
                                 dem[j * dims[0] + i]);
 
+          // compute gradient and check if it's the max gradient
           local_gradient = vertical_dist / horizontal_dist;
           if (local_gradient > max_gradient) {
             max_gradient = local_gradient;
           }
         }
       }
-      // convert results to correct format and save them
-      switch (unit) {
-        case 'r':  // radian
-          output[j * dims[0] + i] = atanf(max_gradient);
-          break;
-        case 'd':  // degree
-          output[j * dims[0] + i] = atanf(max_gradient) * (180.0f / PI);
-          break;
-        case 's':  // sine
-          output[j * dims[0] + i] = sinf(atanf(max_gradient));
-          break;
-        case 'p':  // percent
-          output[j * dims[0] + i] = max_gradient * 100.0f;
-          break;
-        default:  // tangent (default)
-          output[j * dims[0] + i] = max_gradient;
-          break;
-      }
+      // save gradient (tangent)
+      output[j * dims[0] + i] = max_gradient;
     }
   }
 }
